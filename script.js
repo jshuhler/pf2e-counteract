8bc4059a5ecae0f36d005182b6fd6928566188f0
jshuhler <15068877+jshuhler@users.noreply.github.com>
2026-04-07 18:53:53 -0400

    finish styling dark mode and start JS logic

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// ======= _ =======
+// ======= IIFE MAIN FUNCTION =======
+const pfCounteract = (() => {
+    // setting the level of the counteract effect
+    const counteractEffectRankSet = () => {
+        const effectSource = document.getElementById('effect-source');
+        if (effectSource === 'spell') {
+
+        } else if (effectSource === 'other') {
+
+        };
+    };
+
+    // taking in the roll total from Foundry
+    const counteractEffectRollSet = () => {
+        const counteractEffectRoll = {
+            rollValue: document.getElementById('effect-roll').value,
+            noAdjustment: document.getElementById('roll-none').checked ? true : true,
+            naturalOne: (document.getElementById('roll-nat-one').checked) ? true : true,
+            naturalTwenty: (document.getElementById('roll-nat-twenty').checked) ? true : true,
+        }
+    };
+
+
+
+
+
+
+    return {
+        counteractEffectRollSet,
+
+    }
+})();
