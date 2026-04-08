516458f0a556048f6cc510d1a48467bdd324c0b5
jshuhler <15068877+jshuhler@users.noreply.github.com>
2026-04-07 22:53:53 -0400

    set up default dropdown values and hide dropdown based on source

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 // ======= IIFE MAIN FUNCTION =======
 const pfCounteract = (() => {
+
+    // updating the DOM on first IIFE run to hide the level dropdown
+    const effectLevel = document.getElementById('effect-level-container');
+    const targetLevel = document.getElementById('target-level-container');
+    effectLevel.classList.add("hidden");
+    targetLevel.classList.add("hidden");
+
+    // updating the visible dropdown depending on effect source
+    const effectSelect = document.getElementById('effect-source');
+    const effectRank = document.getElementById('effect-rank-container');
+    effectSelect.addEventListener('change', () => {
+        if (effectSelect.value === 'spell') {
+            effectLevel.classList.add("hidden");
+            effectRank.classList.remove("hidden");
+        } else if (effectSelect.value === 'other') {
+            effectRank.classList.add("hidden");
+            effectLevel.classList.remove("hidden");
+        };
+    });
+
+    // updating the visible dropdown depending on target source
+    const targetSelect = document.getElementById('target-source');
+    const targetRank = document.getElementById('target-rank-container');
+    targetSelect.addEventListener('change', () => {
+        if (targetSelect.value === 'spell') {
+            targetLevel.classList.add("hidden");
+            targetRank.classList.remove("hidden");
+        } else if (targetSelect.value === 'other') {
+            targetRank.classList.add("hidden");
+            targetLevel.classList.remove("hidden");
+        };
+    });
+    
     // setting the level of the counteract effect
     const counteractEffectRankSet = () => {
         const effectSource = document.getElementById('effect-source');
@@ -14,19 +47,27 @@ const pfCounteract = (() => {
     const counteractEffectRollSet = () => {
         const counteractEffectRoll = {
             rollValue: document.getElementById('effect-roll').value,
-            noAdjustment: document.getElementById('roll-none').checked ? true : true,
-            naturalOne: (document.getElementById('roll-nat-one').checked) ? true : true,
-            naturalTwenty: (document.getElementById('roll-nat-twenty').checked) ? true : true,
-        }
+            noAdjustment: document.getElementById('roll-none').checked,
+            naturalOne: document.getElementById('roll-nat-one').checked,
+            naturalTwenty: document.getElementById('roll-nat-twenty').checked,
+        };
+        return counteractEffectRoll;
     };
 
+    // setting the level of the counteract target
 
 
+    // setting counteract target dc
+    const counteractTargetDCSet = () => {
+        const counteractTargetDC = document.getElementById('target-dc');
+        return counteractTargetDC;
+    };
 
-
-
-    return {
-        counteractEffectRollSet,
+    // finding degree of success
+    const counteractRollOutcomeSet = (counteractEffectRoll, counteractTargetDC, naturalOne, naturalTwenty) => {
 
     }
+
+
+
 })();
